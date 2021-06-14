e721d51ec89ad2f7c29b6990a6adb530b12cb115
NathanDrakeHat <1136026744@qq.com>
2021-06-14 15:21:38 +0800

    fix primeGen return type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
-import org.nathan.Euler.Difficulty5;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
 
-import static org.nathan.centralUtils.utils.NumericUtils.*;
+import static org.nathan.centralUtils.utils.NumericUtils.primeGen;
 
 public final class Main{
-    public static void main(String[] args){
-        Difficulty5.answer4();
-
+  public static void main(String[] args){
+    long t1 = System.nanoTime();
+    IntArrayList res = new IntArrayList();
+    for(int i = 0; i < 10; i++){
+      res = primeGen(20_000_000);
     }
+    long t2 = System.nanoTime();
+    System.out.println(res.size());
+    System.out.printf("%fms%n", (t2 - t1) / Math.pow(10, 6));
+    System.out.printf("%fms%n", (t2 - t1) / Math.pow(10, 6) / 10.);
+  }
 }
