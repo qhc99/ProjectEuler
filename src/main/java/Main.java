8adc56dd33c6b346e254e12e6ccf456bc965d289
NathanDrakeHat <1136026744@qq.com>
2021-06-21 16:34:29 +0800

    add prime ceiling init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
-import it.unimi.dsi.fastutil.ints.IntArrayList;
+import org.nathan.centralUtils.utils.NumericUtils;
 
-import static org.nathan.centralUtils.utils.NumericUtils.primeGen;
+import java.math.BigDecimal;
+import java.util.List;
 
 public final class Main{
   public static void main(String[] args){
     long t1 = System.nanoTime();
-    IntArrayList res = new IntArrayList();
-    for(int i = 0; i < 10; i++){
-      res = primeGen(20_000_000);
+    List<Integer> res = null;
+    int iter = 10_000;
+    for(int i = 0; i < iter; i++){
+      res = NumericUtils.primeGen(2_000);
     }
     long t2 = System.nanoTime();
     System.out.println(res.size());
     System.out.printf("%fms%n", (t2 - t1) / Math.pow(10, 6));
-    System.out.printf("%fms%n", (t2 - t1) / Math.pow(10, 6) / 10.);
+    System.out.printf("%fus%n", (t2 - t1) / Math.pow(10, 3) / iter);
   }
 }
