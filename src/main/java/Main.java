52767f033b71012d7ba2407a11024f60386ff838
qhc <onlinecqh99@gmail.com>
2024-07-14 01:26:16 -0400

    Add trim

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,94 @@
-import org.nathan.centralUtils.utils.NumericUtils;
+package dev.qhc99.centralibj;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public final class Main{
-  public static void main(String[] args){
-    long t1 = System.nanoTime();
-    List<Integer> res = null;
-    int iter = 10_000;
-    for(int i = 0; i < iter; i++){
-      res = NumericUtils.primeGen(2_000);
+class Main {
+
+    private static BigDecimal generate(List<BigDecimal> a) {
+        var solver = new Object() {
+            public BigDecimal total = BigDecimal.ZERO;
+            private List<BigDecimal> minFutures = new ArrayList<>();
+            private List<BigDecimal> maxFutures = new ArrayList<>();
+
+            {
+                for (int i = 0; i < a.size(); ++i) {
+                    var t = getFutures(i, a);
+                    minFutures.add(t.get(0));
+                    maxFutures.add(t.get(1));
+                }
+            }
+
+            private static List<BigDecimal> getFutures(int idx,
+                    List<BigDecimal> arr) {
+
+                var min = arr.stream().skip(idx).map((a)->{
+                    return a.min(BigDecimal.ONE.subtract(a));
+                }).reduce(BigDecimal::multiply).get();
+
+                var max = arr.stream().skip(idx).map((a)->{
+                    return a.max(BigDecimal.ONE.subtract(a));
+                }).reduce(BigDecimal::multiply).get();
+                return List.of(min, max);
+            }
+
+            private void generateTableRecursive(int current_idx,
+                    BigDecimal prev_choice_1_result,
+                    BigDecimal prev_choice_2_result, List<BigDecimal> arr) {
+                if (current_idx == arr.size()) {
+                    total = total.add(
+                            prev_choice_1_result.max(prev_choice_2_result));
+                } else {
+                    var minFuture = minFutures.get(current_idx);
+                    var maxFuture = maxFutures.get(current_idx);
+                    var minChoice = prev_choice_1_result
+                            .min(prev_choice_2_result);
+                    var maxChoice = prev_choice_2_result
+                            .max(prev_choice_1_result);
+
+                    if (minChoice.multiply(maxFuture)
+                            .compareTo(maxChoice.multiply(minFuture)) <= 0) {
+                        total = total.add(maxChoice);
+                        return;
+                    }
+
+                    generateTableRecursive(current_idx + 1,
+                            prev_choice_1_result.multiply(arr.get(current_idx)),
+                            prev_choice_2_result.multiply(BigDecimal.ONE
+                                    .subtract(arr.get(current_idx))),
+                            arr);
+
+                    generateTableRecursive(current_idx + 1,
+                            prev_choice_1_result.multiply(BigDecimal.ONE
+                                    .subtract(arr.get(current_idx))),
+                            prev_choice_2_result.multiply(arr.get(current_idx)),
+                            arr);
+                }
+            }
+        };
+
+        solver.generateTableRecursive(1, a.get(0),
+                BigDecimal.ONE.subtract(a.get(0)), a);
+        return solver.total;
     }
-    long t2 = System.nanoTime();
-    System.out.println(res.size());
-    System.out.printf("%fms%n", (t2 - t1) / Math.pow(10, 6));
-    System.out.printf("%fus%n", (t2 - t1) / Math.pow(10, 3) / iter);
-  }
+
+    private static List<BigDecimal> bigDecimalArray(int start, int endInclusive,
+            int step, int scale) {
+        var t = IntStream.iterate(start, i -> i <= endInclusive, i -> i + step)
+                .mapToObj(i -> {
+                    return BigDecimal.valueOf(i)
+                            .divide(BigDecimal.valueOf(scale));
+                }).collect(Collectors.toList());
+        return t;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(generate(bigDecimalArray(2, 8, 2, 10)));
+        System.out.println(generate(bigDecimalArray(25, 54, 1, 100)));
+    }
 }
